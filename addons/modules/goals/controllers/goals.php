652289f845857a5e5792d2b51a72f898a4554e32
Mark Jeff Abasula <mabasula@usautoparts.com>
2015-05-15 18:10:35 +0800

    move REST_Controller.php to application/libraries, adding files that part of RESTController

---
+++ addons/modules/goals/controllers/goals.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require(APPPATH.'libraries/REST_Controller.php');
+
+class Goals extends REST_Controller 
+{
+
+	function getGoals_get()
+	{
+
+	}
+
+	function updateGoals_get()
+	{
+
+	}
+
+	function addGoals_get()
+	{
+
+	}
+
+	function deleteGoals_get()
+	{
+
+	}
+
+}
+
+?>
